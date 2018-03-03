4e28c6c6cd6753555a94f0c4699be018b0cf7fe9
maidenluva <bpho345@yahoo.com>
2018-03-03 13:15:32 -0800

    init

---
+++ Version3/app/src/main/java/com/example/binh/version3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.binh.version3;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.location.Location;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
